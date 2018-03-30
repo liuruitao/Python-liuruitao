c438885ef5958943713d260cf410cce6998f20ea
liuruitao <2590011467@qq.com>
2018-03-30 18:10:34 +0800

    变量

---
+++ 06-day/1.py
@@ -0,0 +1,24 @@
+
+name = "刘瑞涛"
+age = "20"
+sex = "男"
+height = "1.77米"
+weight = "60公斤"
+print(title)
+print(name)
+print(age)
+print(sex)
+print(height)
+print(weight)
+
+
+
+height = 18.9
+i_height = int(height)
+ptint(i_height)
+
+double_kill = "2"
+i_double_kill = int(double_kill)
+print(i_double_kill)
+
+
---
+++ 06-day/1.练习.py
@@ -0,0 +1,5 @@
+print("我的名字叫%s,请多多关照！"%name)
+print("我的学号是%06d"%student_no)
+print("西瓜单价%.02f元/斤,购买%.02f斤,需要支付%.02f元"%(price,weight,money))
+print("数据比例是%.02f%%"%(scale*100))
+
